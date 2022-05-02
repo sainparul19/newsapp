20416fc41df34d832a2b0743a270464b85e47fe6
iamparul <himanshu@toothsi.in>
2022-05-02 15:45:02 +0100

    new component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 
 import React, { Component } from "react";
 import NavBar from "./components/NavBar";
+import News from "./components/News";
+import NewsItem from "./components/NewsItem";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <NavBar />
+        <News />
+        <NewsItem />
       </div>
     );
   }
---
+++ src/components/NewsItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    return <div>NewsItem</div>;
+  }
+}
+
+export default NewsItem;
